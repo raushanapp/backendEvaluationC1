fd8715703a74f171b31b3ffd039e87a09f4fed5a
OurSraushan <sraushan890@gmail.com>
2022-03-07 12:10:15 +0530

    all file upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const  app = express();
+
+app.get("/book", (req,res)=>{
+    res.send({ route: "/books"})
+})
+
+app.get("/libraries", checkPermission('librarian'),(req,res)=>{
+    res.send( { route: "/libraries", permission: true})
+})
+app.get("/author", checkPermission('author'),(req,res)=>{
+    res.send( { route: "/author", permission: true})
+})
+
+function checkPermission(data) {
+
+    return function logger(req,res,next) {
+
+        if (data =='author' ||data=='librarian') {
+            return next()
+        }
+
+       return res.send.status(404).send('Data is not found')
+    }
+}
+
+app.listen(1234,()=>{
+    console.log('Listening on Port 1234')
+})
